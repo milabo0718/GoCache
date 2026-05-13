e107b55c601de5eab56bb572d97d5293e2e9650c
sea-urchins-and-chestnuts <1019426464@qq.com>
2026-05-14 02:53:26 +0800

    change singleFlight

--- singleFlight/singleFlight.go
+++ singleFlight/singleFlight.go
@@ -14,22 +14,31 @@ type Group[T any] struct {
 	m  map[string]*call[T]
 }
 
+// singleflight的机制就是要保证对于某个key，同一时间只有一个请求在执行，其他请求等待这个请求完成后直接返回结果。为了实现这个机制，我们需要一个结构体来存储正在执行的请求的信息，包括结果和错误。我们还需要一个互斥锁来保护这个结构体，以确保线程安全。
 func (g *Group[T]) Do(key string, fn func() (T, error)) (T, error) {
+	g.mu.Lock()
+	if g.m == nil {
+		g.m = make(map[string]*call[T])
+	}
+
 	if existingCall, ok := g.m[key]; ok {
+		g.mu.Unlock()
 		existingCall.wg.Wait()
 		return existingCall.val, existingCall.err
 	}
-	g.mu.Lock()
-	defer g.mu.Unlock()
 
-	val, err := fn()
-	c := &call[T]{val: val, err: err}
+	c := &call[T]{}
 	c.wg.Add(1)
 	g.m[key] = c
+	g.mu.Unlock()
+	c.val, c.err = fn()
 	c.wg.Done()
+
+	g.mu.Lock()
 	// 删除key
 	delete(g.m, key)
+	g.mu.Unlock()
 
-	return val, err
+	return c.val, c.err
 
 }
